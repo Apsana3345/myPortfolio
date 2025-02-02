eaceede1be8f956c98a4d3b2ea52dafc25bef7bd
Apsana <apsana20580912@gmail.com>
2025-02-02 15:35:40 +0545

    resumeAdded

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
             </section>
             <section>
               <button className="resume-btn">
-                <a href="https://drive.google.com/file/d/1KNTHjwPmsmBf79eK3vGa-JnlulWxrVCV/view?usp=drive_link">Resume</a>
+                <a href="https://drive.google.com/file/d/1KNTHjwPmsmBf79eK3vGa-JnlulWxrVCV/view?usp=drive_link" target="_blank">Resume</a>
                 
                 </button>
             </section>
